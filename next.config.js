f9b93946649b323b27459b3a3b9030bf4b418d07
tushat velotio <tushar.tibude@velotio.com>
2022-07-28 13:40:38 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const nextConfig = {
   experimental: {
     outputStandalone: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/fund',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:'public, s-maxage=10, stale-while-revalidate=59',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
